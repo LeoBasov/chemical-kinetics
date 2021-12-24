095f2f770669ff074f7bcbcbd7946438edc73667
Leo Basov <you@example.com>
2021-12-24 14:32:44 +0100

    Added dummy files.

---
+++ test/unit_tests/dummy.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include "../../src/dummy.h"
+
+namespace hamt {
+namespace gmsh {
+
+TEST(dummy, foo) {}
+
+TEST(dummy, ba) {}
+
+}  // namespace gmsh
+}  // namespace hamt
