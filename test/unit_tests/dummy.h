a220dc6c73b6d6265be9e89194656eed9fd4a752
Leo Basov <you@example.com>
2021-12-24 14:40:35 +0100

    Fixed bug in main.

--- test/unit_tests/dummy.h
+++ test/unit_tests/dummy.h
@@ -4,12 +4,10 @@
 
 #include "../../src/dummy.h"
 
-namespace hamt {
-namespace gmsh {
+namespace chem {
 
 TEST(dummy, foo) { ASSERT_EQ(0, foo()); }
 
 TEST(dummy, ba) { ASSERT_EQ(12.1, ba()); }
 
-}  // namespace gmsh
-}  // namespace hamt
+}  // namespace chem
