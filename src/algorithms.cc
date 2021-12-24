87312aaf7d74340150da13210569930ed529ead0
Leo Basov <you@example.com>
2021-12-24 16:11:04 +0100

    Added exception. Added algorithm for reaction rate calculation.

--- src/algorithms.cc
+++ src/algorithms.cc
@@ -7,5 +7,23 @@ double arrhenius(const double& pre_exp_factor, const double& activation_energy, 
     return pre_exp_factor * std::exp(-activation_energy / (constants::R * temperature));
 }
 
+VectorXd calc_reaction_rates(const VectorXd& rate_constants, const VectorXd& concentrations,
+                             const MatrixXd& reaction_powers) {
+    VectorXd reaction_rates(rate_constants);
+
+    if (reaction_rates.size() * concentrations.size() != reaction_powers.size()) {
+        throw Exception("arrays of incompatible size " + std::to_string(reaction_rates.size() * concentrations.size()),
+                        __PRETTY_FUNCTION__);
+    }
+
+    for (long j = 0; j < reaction_rates.size(); j++) {
+        for (long i = 0; i < concentrations.size(); i++) {
+            reaction_rates(j) *= std::pow(concentrations(i), reaction_powers(j, i));
+        }
+    }
+
+    return reaction_rates;
+}
+
 }  // namespace algorithms
 }  // namespace chem
--- src/algorithms.h
+++ src/algorithms.h
@@ -1,14 +1,21 @@
 #pragma once
 
+#include <Eigen/Dense>
 #include <cmath>
+#include <iostream>
+#include <vector>
 
 #include "constants.h"
+#include "exception/exception.h"
 
 namespace chem {
 namespace algorithms {
+using namespace Eigen;
 
 // [activation_energy] = R⋅T
 double arrhenius(const double& pre_exp_factor, const double& activation_energy, const double& temperature);
+VectorXd calc_reaction_rates(const VectorXd& rate_constants, const VectorXd& concentrations,
+                             const MatrixXd& reaction_powers);
 
 }  // namespace algorithms
 }  // namespace chem
