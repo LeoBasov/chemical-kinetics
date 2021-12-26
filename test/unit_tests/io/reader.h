1bb5a1744e02c18de803a4eebba3080f27b11b07
Leo Basov <you@example.com>
2021-12-26 20:21:54 +0100

    Extended unit tests.

--- test/unit_tests/io/reader.h
+++ test/unit_tests/io/reader.h
@@ -6,7 +6,7 @@
 
 namespace chem {
 
-TEST(Reader, read_file) {
+TEST(Reader, get_time_step) {
     const std::string wrong_file_name("wrong_name.in");
     const std::string file_name_const_dt("./test/unit_tests/test_data/reader/reader_dt_cont.in");
     const std::string file_name_var_dt("./test/unit_tests/test_data/reader/reader_dt_var.in");
@@ -22,6 +22,36 @@ TEST(Reader, read_file) {
 
     ASSERT_EQ(TimeStep::VARIABLE, reader.get_time_step().type);
     ASSERT_DOUBLE_EQ(1e-7, reader.get_time_step().value);
+}
+
+TEST(Reader, get_thermal) {
+    const std::string file_name("./test/unit_tests/test_data/reader/reader_dt_var.in");
+    Reader reader;
+    Thermal thermal;
+
+    reader.read_file(file_name);
+
+    thermal = reader.get_thermal();
+
+    ASSERT_EQ(Thermal::CONSTANT, thermal.type);
+    ASSERT_DOUBLE_EQ(2000, thermal.value);
+}
+
+TEST(Reader, get_species_name) {
+    const std::string file_name("./test/unit_tests/test_data/reader/reader_dt_var.in");
+    Reader reader;
+    std::vector<std::string> species_names;
+
+    reader.read_file(file_name);
+
+    species_names = reader.get_species_name();
+
+    ASSERT_EQ(4, species_names.size());
+
+    ASSERT_EQ("H2O", species_names.at(0));
+    ASSERT_EQ("O2", species_names.at(1));
+    ASSERT_EQ("H2", species_names.at(2));
+    ASSERT_EQ("CO2", species_names.at(3));
 }
 
 }  // namespace chem
