730267252f2650ecc1490c96b5168953c5fb5381
Leo Basov <leo.basov@dlr.de>
2022-01-03 10:51:30 +0100

    fixed reaction printing

--- src/io/sc_print_algorithms.cc
+++ src/io/sc_print_algorithms.cc
@@ -1,4 +1,4 @@
-#include "sc_print_algorithms.h"
+﻿#include "sc_print_algorithms.h"
 
 namespace chem {
 namespace sc_print_algorithms {
@@ -27,16 +27,21 @@ void print_species(const Species& species) {
 }
 
 void print_reactions(const std::vector<Reaction>& reactions, const Species& species) {
+    const size_t max_educt_size(get_max_educt_size(reactions));
+    const size_t max_prduct_size(get_max_product_size(reactions));
+
     for (size_t i = 0; i < reactions.size(); i++) {
-        print_reaction(reactions.at(i), species);
+        print_reaction(reactions.at(i), species, max_educt_size, max_prduct_size);
     }
 
     std::cout << std::endl;
 }
 
-void print_reaction(const Reaction& reaction, const Species& species) {
+void print_reaction(const Reaction& reaction, const Species& species, const size_t& max_educt_size,
+                    const size_t& max_product_size) {
     bool first(true);
-    size_t max_name_l(species.get_max_name_size());
+    size_t max_name_l(get_max_name_size(species));
+    size_t length(0);
 
     std::cout << tab(tab_size) << "reaction: ";
     std::cout.precision(1);
@@ -53,8 +58,12 @@ void print_reaction(const Reaction& reaction, const Species& species) {
         std::cout << std::scientific << reaction.educt_stoichiometric_coefficients.at(i) << "⋅" << species.names.at(i)
                   << tab(max_name_l - species.names.at(i).size());
         first = false;
+        length++;
     }
 
+    std::cout << tab((11 + max_name_l) * (max_educt_size - length));
+
+    length = 0;
     first = true;
     std::cout << " →  ";
 
@@ -70,7 +79,10 @@ void print_reaction(const Reaction& reaction, const Species& species) {
         std::cout << reaction.product_stoichiometric_coefficients.at(i) << "⋅" << species.names.at(i)
                   << tab(max_name_l - species.names.at(i).size());
         first = false;
+        length++;
     }
+
+    std::cout << tab((11 + max_name_l) * (max_product_size - length));
 
     if (reaction.rate_constant.type == RateConstant::CONSTANT) {
         std::cout << " : k = CONST";
@@ -79,6 +91,62 @@ void print_reaction(const Reaction& reaction, const Species& species) {
     }
 
     std::cout << std::endl;
+}
+
+size_t get_max_name_size(const Species& species) {
+    size_t max_name_l(0);
+
+    for (const auto& name : species.names) {
+        if (name.size() > max_name_l) {
+            max_name_l = name.size();
+        }
+    }
+
+    return max_name_l;
+}
+
+size_t get_max_educt_size(const std::vector<Reaction>& reactions) {
+    size_t educt_size(0);
+
+    for (const auto& reaction : reactions) {
+        size_t loc_size(0);
+
+        for (size_t i = 0; i < reaction.educt_stoichiometric_coefficients.size(); i++) {
+            if (!reaction.educt_stoichiometric_coefficients.at(i)) {
+                continue;
+            }
+
+            loc_size++;
+        }
+
+        if (loc_size > educt_size) {
+            educt_size = loc_size;
+        }
+    }
+
+    return educt_size;
+}
+
+size_t get_max_product_size(const std::vector<Reaction>& reactions) {
+    size_t product_size(0);
+
+    for (const auto& reaction : reactions) {
+        size_t loc_size(0);
+
+        for (size_t i = 0; i < reaction.product_stoichiometric_coefficients.size(); i++) {
+            if (!reaction.product_stoichiometric_coefficients.at(i)) {
+                continue;
+            }
+
+            loc_size++;
+        }
+
+        if (loc_size > product_size) {
+            product_size = loc_size;
+        }
+    }
+
+    return product_size;
 }
 
 std::string tab(const size_t& n) {
--- src/io/sc_print_algorithms.h
+++ src/io/sc_print_algorithms.h
@@ -11,7 +11,12 @@ const size_t tab_size(4);
 
 void print_species(const Species& species);
 void print_reactions(const std::vector<Reaction>& reactions, const Species& species);
-void print_reaction(const Reaction& reaction, const Species& species);
+void print_reaction(const Reaction& reaction, const Species& species, const size_t& max_educt_size,
+                    const size_t& max_product_size);
+
+size_t get_max_name_size(const Species& species);
+size_t get_max_educt_size(const std::vector<Reaction>& reactions);
+size_t get_max_product_size(const std::vector<Reaction>& reactions);
 
 std::string tab(const size_t& n);
 std::string tab();
